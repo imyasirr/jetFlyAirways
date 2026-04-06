bd5e6f66d7eec3527179b2f15ece45ba0f8c7910
Yasir Hussain <icloud.yasir@icloud.com>
2026-04-07 00:52:21 +0530

    api integration added

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Blog;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
 
 class BlogController extends Controller
 {
@@ -25,6 +26,17 @@ class BlogController extends Controller
             $blog->publish_at !== null && $blog->publish_at->lte(now()),
             404
         );
+
+        // ✅ IMAGE URL FIX YAHAN
+        if ($blog->cover_url) {
+            if (Str::startsWith($blog->cover_url, ['http://', 'https://'])) {
+                $blog->cover_image = $blog->cover_url;
+            } else {
+                $blog->cover_image = asset('storage/' . $blog->cover_url);
+            }
+        } else {
+            $blog->cover_image = null;
+        }
 
         return view('blog.show', compact('blog'));
     }
